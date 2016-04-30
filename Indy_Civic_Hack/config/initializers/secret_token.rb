267165b3b774aa28e3a2d5597431b58c83397513
andrewariley87 <andrewariley87@gmail.com>
2016-04-29 21:17:54 -0400

    creates rails app

---
+++ Indy_Civic_Hack/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+IndyCivicHack::Application.config.secret_key_base = 'adfe257f74375749db5c845e76e3f953c8c7fab5a03930df554c073a5e4a874ace408ed5c7f268d6b6ab670ded8e69cfb47cade7e25535cd6768291ffb281fd0'
---
+++ Indy_Civic_Hack/config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+IndyCivicHack::Application.config.session_store :cookie_store, key: '_Indy_Civic_Hack_session'
