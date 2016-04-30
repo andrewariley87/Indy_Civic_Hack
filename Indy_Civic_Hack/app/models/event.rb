c9eda661283171f850cf94cc2bf235be66cbb0bf
andrewariley87 <andrewariley87@gmail.com>
2016-04-30 12:48:12 -0400

    fixes bad data in the seed

--- Indy_Civic_Hack/app/models/event.rb
+++ Indy_Civic_Hack/app/models/event.rb
@@ -3,14 +3,7 @@ class Event < ActiveRecord::Base
   geocoded_by :full_address
   after_validation :geocode
 
-  validates_prescence_of :city_state_address,
-                         :address
-
-  # def full_address
-  #   if city_state_address.exclude(", IN")
-  #     address + " " + "Indiana"
-  #   else
-  #     address + " " + city_state_address
-  #   end
-  # end
+  def full_address
+    address + " " + city_state_zip
+  end
 end
