35e7d46d71713d413eeeb4526b27f1a9919680a9
tsherlock86 <tadsherlock@gmail.com>
2016-04-30 15:18:39 -0400

    infowindow and start of select

--- Indy_Civic_Hack/app/controllers/events_controller.rb
+++ Indy_Civic_Hack/app/controllers/events_controller.rb
@@ -5,6 +5,10 @@ class EventsController < ApplicationController
     @hash = Gmaps4rails.build_markers(@mapevents) do |event, marker|
       marker.lat event.latitude
       marker.lng event.longitude
+
+      marker.infowindow render_to_string(:partial => "/events/infowindow", :locals => { :event => event })
+          marker.title "#{event.case}"
+          marker.json({ :business => event.business})
     end
   end
 
