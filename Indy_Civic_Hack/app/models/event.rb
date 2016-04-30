e5aa12c73deed813927305dd9fdb1d7ccef692c6
andrewariley87 <andrewariley87@gmail.com>
2016-04-30 12:17:09 -0400

    WIP: full_address

--- Indy_Civic_Hack/app/models/event.rb
+++ Indy_Civic_Hack/app/models/event.rb
@@ -3,9 +3,14 @@ class Event < ActiveRecord::Base
   geocoded_by :full_address
   after_validation :geocode
 
-  def full_address
-    address + " " + city_state_zip
-  end
+  validates_prescence_of :city_state_address,
+                         :address
 
-
+  # def full_address
+  #   if city_state_address.exclude(", IN")
+  #     address + " " + "Indiana"
+  #   else
+  #     address + " " + city_state_address
+  #   end
+  # end
 end
